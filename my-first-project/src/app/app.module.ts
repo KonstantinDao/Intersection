9170e576950f795ffe4787846cfe9de2d26dd980
mika.bender <mika.bender@rwth-aachen.de>
2023-01-20 15:00:00 +0100

    Buttons und textfelder richtig plaziert und gleichlang

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { LoginEingabeComponent } from './login-eingabe/login-eingabe.component';
 import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginButtonComponent } from './login-eingabe/login-button/login-button.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+//import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,14 +22,22 @@ import { MatFormFieldModule} from '@angular/material/form-field';
     AppComponent,
     HeaderComponent,
     LoginEingabeComponent,
+    LoginButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+//    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ my-first-project/src/app/login-eingabe/login-button/login-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-button',
+  templateUrl: './login-button.component.html',
+  styleUrls: ['./login-button.component.css']
+})
+export class LoginButtonComponent {}
