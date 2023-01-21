40701610649aa11cde5066d6d38cd4f337684796
mika.bender <mika.bender@rwth-aachen.de>
2023-01-21 15:47:43 +0100

    anfänge intressensauswahl

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MatchingHistoryComponent } from './matching-history/matching-history.co
 import { ChatComponent } from './chat/chat.component';
 import { NatchingResultsComponent } from './natching-results/natching-results.component';
 import { SignupEingabeComponent } from './signup-eingabe/signup-eingabe.component';
+import {MatChipsModule} from '@angular/material/chips';
 
 const meineRouten: Routes = [
   {path: '', component: LoginEingabeComponent},
@@ -67,6 +68,8 @@ const meineRouten: Routes = [
     MatSelectModule,
     MatRadioModule,
     MatCardModule,
+    MatChipsModule,
+
 //    ReactiveFormsModule
 ],
 exports: [
