18af42ee964d8e2f13045da19a020f740e9c9e90
anna.bott <anna.bott@rwth-aachen.de>
2023-01-21 14:58:00 +0100

    createroom page, menu page

--- my-first-project/src/app/create-room/create-room.component.ts
+++ my-first-project/src/app/create-room/create-room.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-room.component.css']
 })
 export class CreateRoomComponent {
-
+  submit(){
+    document.location.href = "/room";
+   }
 }
