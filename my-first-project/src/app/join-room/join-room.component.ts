3816ffde9b6ae14bb00768e8c12db0b833f484a9
anna.bott <anna.bott@rwth-aachen.de>
2023-01-21 15:36:36 +0100

    joinRoom page

--- my-first-project/src/app/join-room/join-room.component.ts
+++ my-first-project/src/app/join-room/join-room.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./join-room.component.css']
 })
 export class JoinRoomComponent {
-
+  joinroom(){
+    document.location.href = "/room";
+   }
 }
