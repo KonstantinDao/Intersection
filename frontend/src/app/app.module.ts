f2a70e9d76043a2895230de5f4e4dfbbd5baf984
KonstantinDao <konstantin.dao@outlook.de>
2023-01-25 18:17:57 +0100

    login works

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -62,6 +63,8 @@ const meineRouten: Routes = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatFormFieldModule,
     MatSlideToggleModule,
     MatInputModule,
--- frontend/src/app/signup-page/signup-page.component.ts
+++ frontend/src/app/signup-page/signup-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { User } from './User';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-signup-page',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-page.component.css']
 })
 export class SignupPageComponent {
+
+  readonly ROOT_URL = 'http://localhost:8080/api'
+
+
+  users: any
+  newUser: any
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    this.users = this.http.get<User[]>(this.ROOT_URL + '/users');
+  }
+
+  createUser() {
+    document.location.href = "/menu";
+    var username = (<HTMLInputElement>document.getElementById('username')).value;
+    var password = (<HTMLInputElement>document.getElementById('password')).value;
+
+    const data: User = {
+      id: '',
+      name: username,
+      password: password,
+      interests: [],
+      matchingHistory: []
+    }
+
+    this.newUser = this.http.post(this.ROOT_URL + '/users', data)
+  
+  }
+
 
 }
