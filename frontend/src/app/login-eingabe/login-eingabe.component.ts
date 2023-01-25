7f3a7243a4d57893222a8970b1403b6469f30a89
KonstantinDao <konstantin.dao@outlook.de>
2023-01-25 19:25:35 +0100

    beginningLogin

--- frontend/src/app/login-eingabe/login-eingabe.component.ts
+++ frontend/src/app/login-eingabe/login-eingabe.component.ts
@@ -11,5 +11,24 @@ export class LoginEingabeComponent {
     fehler(){
       document.getElementsByTagName('p')[0].innerHTML = "Something didn't work. Please try again.";
     }
+    checkUser() {
+
+      var username = (<HTMLInputElement>document.getElementById('username')).value;
+      var password = (<HTMLInputElement>document.getElementById('password')).value;
+  
+      const data: User = {
+        id: '',
+        name: username,
+        password: password,
+        interests: [],
+        matchingHistory: []
+      }
+  
+      this.http.post(this.ROOT_URL + '/users', data).subscribe(data => {
+        console.log(data);
+        this.user = data;
+      })
+      document.location.href = "/menu";
+    }
 }
 
